c6927f83dc87f24fbf2aa8990b4839feee881419
Gabriel Noguchi <gabriel.noguchi@soaexpert.com.br>
2017-04-17 18:51:24 -0300

    atualizações treinamento

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   restaurant: {name:'Our Awesome restaurant', yearsOpen: 1 },
+  newItem:null,
+  availableItems: Ember.computed.filterBy('model','isAvailable',true),
   actions : {
       makeUnavailable(food){
         Ember.set(food, 'isAvailable', false);
@@ -10,6 +12,16 @@ export default Ember.Controller.extend({
       makeAvailable(food){
         Ember.set(food,'isAvailable', true);
         food.save();
+      },
+      saveNewItem(){
+        this.store.createRecord('food', {
+           isAvailable:false,
+           name: this.get('newItem')
+        }).save()
+        this.set('newItem', '')
+      },
+      destroyItem(food){
+        food.destroyRecord();
       }
   }
 });
